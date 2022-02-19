bab80be118da01ea99a4260cf1d95ff8394318fa
Türker Karapetti <trkrkrl53@gmail.com>
2022-02-19 09:40:47 +0300

    17.gun-1. adim sonu

---
+++ Readme-adimlar.txt
@@ -0,0 +1,10 @@
+17 Gün ödevi
+
+İlk olarak dosya olusturuldu
+app icerisine new folder- directives
+app-icerisinde  new folder components
+app icerisindeki appcomponent.html deki kodlari sildik enalttaki root haric
+Component'i terminalde ac ve terminal vasitasiyla componentleri ekle
+ng g component ....componentadineyse  sekline ekle
+componentler:brand,car,color,customer,rental ve navi
+---17.gun-1. adim sonu--
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './Components/brand/brand.component';
+import { CarComponent } from './Components/car/car.component';
+import { ColorComponent } from './Components/color/color.component';
+import { CustomerComponent } from './Components/customer/customer.component';
+import { RentalComponent } from './Components/rental/rental.component';
+import { NaviComponent } from './Components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    CarComponent,
+    ColorComponent,
+    CustomerComponent,
+    RentalComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
